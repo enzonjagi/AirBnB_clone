4b221e2ed082c9a0cb260ef365de5adbf71530b1
StephenMakenziWaweru <stevomakenzi@gmail.com>
2021-11-10 15:33:18 +0300

    store first object

---
+++ models/engine/file_storage.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""File storage module"""
+import json
+
+
+class FileStorage():
+    """
+    Serializes instances to a JSON file and deserializes JSON file to instances
+    """
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """Return the dictionary __objects"""
+        return FileStorage.__objects
+
+    def new(self, obj):
+        """Sets in __objects the obj with key <obj class name>.id"""
+        k = "{}.{}".format(type(obj).__name__, obj.id)
+        # key = "{}.{}".format(obj["__class__"], obj["id"])
+        FileStorage.__objects[k] = obj
+
+    def save(self):
+        """Serializes __objects to the JSON file"""
+        with open(FileStorage.__file_path, "w", encoding='utf-8') as f:
+            dict = {k: v.to_dict() for k, v in FileStorage.__objects.items()}
+            json.dump(dict, f)
+
+    def imports(self):
+        """Returns the class Basemodel to avoid circular import"""
+        from models.base_model import BaseModel
+
+        return BaseModel
+
+    def reload(self):
+        """deserializes the JSON file to __objects"""
+        try:
+            with open(FileStorage.__file_path, "r", encoding='utf-8') as f:
+                dict = json.load(f)
+                dict = {k: self.imports()(**v) for k, v in dict.items()}
+                FileStorage.__objects = dict
+                # print("->",FileStorage.__objects)
+        except:
+            pass
