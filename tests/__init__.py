4d1b9b997c21406242a47d8cb74949c36d851236
enzonjagi <amonsdungo@gmail.com>
2021-11-11 14:01:49 +0300

    Minimal changes

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,10 +1,10 @@
-#!/usr/bin/python3
-"""test folder __init__ module"""
-from .models.engine.file_storage import FileStorage
-from .models.base_model import BaseModel
-from .console import HBNBCommand
+# #!/usr/bin/python3
+# """test folder __init__ module"""
+# from ..models.engine.file_storage import FileStorage
+# from ..models.base_model import BaseModel
+# from ..console import HBNBCommand
 
-storage = FileStorage()
-storage.reload()
-hbnb = HBNBCommand()
-base = BaseModel()
+# storage = FileStorage()
+# storage.reload()
+# hbnb = HBNBCommand()
+# base = BaseModel()
