d00a8948507db503826e7129471fccffffa46f5d
enzonjagi <amonsdungo@gmail.com>
2021-11-17 21:57:13 +0300

    tests

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -46,8 +46,7 @@ class TestBaseModel(unittest.TestCase):
         """
         self.assertTrue(hasattr(self.base_model, "created_at"))
         self.assertTrue(hasattr(self.base_model, "updated_at"))
-        self.assertEqual(self.base_model.created_at,
-                               self.base_model.updated_at)
+        
         datenow = datetime.now()
         self.testmodel = BaseModel()
         self.assertAlmostEqual(self.testmodel.created_at, datenow)
